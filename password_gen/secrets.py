2a615697a8fb5966feb907e7685c41fac5d82942
Augustine Abimbola <augustineabimbola@Augustines-MacBook-Pro.local>
2020-08-18 10:52:26 -0400

    testing

--- password_gen/secrets.py
+++
@@ -1,2 +0,0 @@
-
-SECRET_KEY = '*1th!^&2+78y2_8&+#@3ip!pa17dwiiy+@*pkrjvote#0mtxhg'
--- password_gen/settings.py
+++ password_gen/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from .secrets import SECRET_KEY
+import secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = '*1th!^&2+78y2_8&+#@3ip!pa17dwiiy+@*pkrjvote#0mtxhg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
